b0bdb9c71eafda4976e527c536b2da7068819474
Alexander Evseev <testevseev@yandex.ru>
2023-05-23 20:49:08 +0300

    First_code

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите число №1: ");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("Введите число №2: ");
+        int b = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма:" + (a+b));
+        System.out.println("Разность:" + (a-b));
+        System.out.println("Произведение:" + (a*b));
+        System.out.println("Частное:" + (double)a/b);
     }
 }
